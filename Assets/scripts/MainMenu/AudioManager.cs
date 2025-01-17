2521f2dfae48e32d55dbf7b76e84e1fb23671e7a
Rumsse <mwieczorek5@edu.cdv.pl>
2025-01-17 13:52:45 +0100

    Wprowadzenie do Audio
    dodałam jakieś różne background music dla testów + stworzenie AudioManager

---
+++ Assets/scripts/MainMenu/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource soundsSource;
+
+    public AudioClip walkingMC;
+    public AudioClip flyingBAT;
+
+    private void Start()
+    {
+        
+    }
+
+    public void PlaySounds()
+    {
+        
+    }
+
+}
