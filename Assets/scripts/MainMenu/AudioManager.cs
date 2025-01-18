c7e3d9bb640e487f0178cb20bd7c36444fc34c36
Rumsse <mwieczorek5@edu.cdv.pl>
2025-01-18 07:23:05 +0100

    dodanie music slider do options panel

--- Assets/scripts/MainMenu/AudioManager.cs
+++ Assets/scripts/MainMenu/AudioManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource soundsSource;
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musicSlider;
 
     public AudioClip walkingMC;
     public AudioClip flyingBAT;
@@ -18,6 +23,12 @@ public class AudioManager : MonoBehaviour
     public void PlaySounds()
     {
         
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(volume)*20);
     }
 
 }
